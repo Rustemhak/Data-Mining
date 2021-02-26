5ead914d61d7165d292a5e88eeece6a4b9255dbb
Rustemhak <rustemhak@mail.ru>
2021-02-26 17:13:04 +0300

    fix adding data to db

--- python/web-crawler.py
+++ python/web-crawler.py
@@ -2,13 +2,13 @@ import requests
 import psycopg2
 
 
-def take_200_posts(domain):
+def take_posts(domain, count_post):
     token = '378645243786452437864524f337f0dea0337863786452457b19ce45a551858c0473f14'
     version = 5.126
     offset = 0
     all_posts = []
 
-    while offset < 200:
+    while offset < count_post:
         response = requests.get('https://api.vk.com/method/wall.get', params={
             'access_token': token,
             'v': version,
@@ -20,12 +20,13 @@ def take_200_posts(domain):
         data = response.json()['response']['items']
         offset += 100
         all_posts.extend(data)
+
     return all_posts
 
 
-def create_dict_count_word(domain):
+def create_dict_count_word(domain, count_post):
     dictionary = {}
-    all_posts = take_200_posts(domain)
+    all_posts = take_posts(domain, count_post)
     for post in all_posts:
         text = post['text']
         words = text.split()
@@ -37,20 +38,7 @@ def create_dict_count_word(domain):
     return dictionary
 
 
-def get_top_100_word(domain):
-    count = 0
-    dictionary = create_dict_count_word(domain)
-    top_words = []
-    for top_word in sorted(dictionary, reverse=True, key=lambda w: dictionary[w]):
-        if count <= 100:
-            top_words.append([top_word, dictionary[top_word]])
-            count += 1
-        else:
-            break
-    return top_words
-
-
-def add_to_db_top_100_word(group_name):
+def add_to_db_count_word(group_name, count_post):
     con = psycopg2.connect(
         database="postgres",
         user="postgres",
@@ -59,17 +47,18 @@ def add_to_db_top_100_word(group_name):
         port="5432"
     )
     cur = con.cursor()
-    cur.execute("truncate table top_100_words")
+    cur.execute("truncate table count_words")
     con.commit()
     print("Database opened successfully")
-    top_100_words = get_top_100_word(group_name)
-    for item in top_100_words:
-        values = {'name': item[0], 'count': item[1]}
-        cur.execute("insert into top_100_words (name, count) values (%(name)s,%(count)s)",
+    count_words = create_dict_count_word(group_name, count_post)
+    for name in count_words.keys():
+        values = {'name': name, 'count': count_words[name]}
+        cur.execute("insert into count_words (name, count) values (%(name)s,%(count)s)",
                     values
                     )
     con.commit()
 
 
 group_name = 'itis_kfu'
-add_to_db_top_100_word(group_name)
+count_post = 200
+add_to_db_count_word(group_name, count_post)
